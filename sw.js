57f0e3a3504bf2a2fd0ccf4ddec49c9329df3a1e
wackywaynesa-blip <wackywaynesa@gmail.com>
2026-04-22 18:38:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'adv-control-v2';
+const CACHE_NAME = 'adv-control-v3';
 const ASSETS = [
     './index.html',
     './manifest.json',
@@ -8,18 +8,37 @@ const ASSETS = [
     'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap'
 ];
 
+// Install: Cache essential assets
 self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Force the new service worker to take over immediately
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// Activate: Clean up old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
         })
     );
 });
 
+// Fetch: Stale-While-Revalidate strategy
+// Load from cache for speed, but fetch from network to update cache for next time
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.match(event.request).then((response) => {
+                const fetchPromise = fetch(event.request).then((networkResponse) => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+                return response || fetchPromise;
+            });
         })
     );
 });
