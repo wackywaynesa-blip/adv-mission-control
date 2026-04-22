ef548d7573b87028b691e67e8c32f450cd6ecf6d
wackywaynesa-blip <wackywaynesa@gmail.com>
2026-04-22 18:18:35 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'adv-control-v2';
+const ASSETS = [
+    './index.html',
+    './manifest.json',
+    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+    'https://cdn.jsdelivr.net/npm/chart.js',
+    'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
