0bced59708558c82df33d8be2d84074a82be7dc5
wackywaynesa-blip <wackywaynesa@gmail.com>
2026-04-22 19:59:54 +0200

    Add files via upload

--- ANDROID/sw.js
+++ ANDROID/sw.js
@@ -1,17 +1,36 @@
-const CACHE_NAME = 'adv-hud-v1';
+const CACHE_NAME = 'adv-hud-v2';
 const ASSETS = [
     './hud.html',
     './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Force update immediately
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
 });
 
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.match(event.request).then((response) => {
+                const fetchPromise = fetch(event.request).then((networkResponse) => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+                return response || fetchPromise;
+            });
+        })
     );
 });
